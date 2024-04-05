d34891293b8374c63c651e80f7437266d1d9b622
Kavisha-de-silva <hkavisha0415@gmail.com>
2024-04-05 21:37:49 +0530

    Updates

---
+++ Script.js
@@ -0,0 +1,6 @@
+function loginUser() {
+    // Assuming login is successful
+    // Redirect user to the workstation page
+    window.location.href = "workstation.html";
+    return false; // Prevent form from submitting
+}
